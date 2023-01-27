29a4b2ae90b7f1795faf5f9108d84e67a125bbab
Davy133 <55928285+Davy133@users.noreply.github.com>
2023-01-27 12:29:49 -0300

    newline implementation

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -26,18 +26,25 @@ enum vga_color{
 	VGA_COLOR_WHITE = 15,
 };
 
-static inline uint8 vga_entry_color(enum vga_color fg, enum vga_color bg)
-{
-    return fg | bg << 4;
-}
-static inline uint16 vga_entry(unsigned char uc,uint8 color)
-{
-    return (uint16) uc | (uint16) color << 8;
-}
 size_t terminal_row;
 size_t terminal_column;
 uint8 terminal_color;
 uint16* terminal_buffer;
+
+static inline uint8 vga_entry_color(enum vga_color fg, enum vga_color bg)
+{
+    return fg | bg << 4;
+}
+
+static inline uint16 vga_entry(unsigned char uc,uint8 color)
+{
+    return (uint16) uc | (uint16) color << 8;
+}
+
+static inline void jump_nextline(){
+    terminal_row++;
+    terminal_column = 0;
+}
 
 void terminal_initialize(void)
 {
@@ -52,6 +59,7 @@ void terminal_initialize(void)
         }
     }
 }
+
 void terminal_setcolor(uint8 color){
     terminal_color = color;
 }
@@ -71,11 +79,18 @@ void terminal_putchar(char c)
 			terminal_row = 0;
 	}
 }
- 
+
 void terminal_write(const char* data, size_t size) 
 {
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
+	for (size_t i = 0; i < size; i++){
+        if(data[i] == '\n'){
+            terminal_putchar('\0');
+            jump_nextline();
+        }else{
+            terminal_putchar(data[i]);
+        }
+		
+    }
 }
  
 void terminal_writestring(const char* data) 
@@ -86,6 +101,7 @@ void terminal_writestring(const char* data)
 extern void main(){
     
     terminal_initialize();
-    terminal_writestring("Hello World!");
-
+    terminal_writestring("Hello World!\n");
+    terminal_writestring("Hello World!\n");
+    terminal_writestring("AEMRLO\n");
 }
